d3a7a676834ae494585393dc1d4718badb335be5
mail21 <ismailidn1@gmail.com>
2020-08-14 17:08:45 +0700

    Add Sign In Sign up pages

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.style.scss';
+import SignIn from './../../components/sign-in/sign-in.component';
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <h1>SIGN IN</h1>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
