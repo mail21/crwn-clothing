c7778ba358fa0a4c4eaded226904e2a9942f2328
mail21 <ismailidn1@gmail.com>
2020-08-15 12:16:21 +0700

    Implementasi sign in menggunakan google

--- src/App.js
+++ src/App.js
@@ -5,18 +5,44 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signIn" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+    //auth.onAuthStateChanged itu bersifat open subscription
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+    //meng-close subscription
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signIn" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.style.scss';
 import { ReactComponent as Logo } from './../../assets/crown.svg';
+import { auth } from './../../firebase/firebase.utils';
 
-function Header() {
+function Header({ currentUser }) {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -11,11 +12,20 @@ function Header() {
       </Link>
       <div className="options">
         <Link className="option" to="/shop">
-          Shop
+          SHOP
         </Link>
         <Link className="option" to="/contact">
-          Contact
+          CONTACT
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signIn">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
