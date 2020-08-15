70da6f097a553555cf5f977b053de76246e2017c
mail21 <ismailidn1@gmail.com>
2020-08-15 14:53:21 +0700

    Delete console log

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import './form-input.style.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => {
-  console.log('forminput', otherProps);
   return (
     <div className="group">
       <input type="email" className="form-input" onChange={handleChange} {...otherProps} />
-      {label ? <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>{label}</label> : ''}
+      {label ? (
+        <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+          {label}
+        </label>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
