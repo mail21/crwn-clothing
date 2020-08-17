be0b7fec4bd6239c0ffbfbfd5c7393ffee240807
mail21 <ismailidn1@gmail.com>
2020-08-17 14:12:23 +0700

    Menggabungkan cart reducer ke rootReducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user.reducer/user.reducer';
+import cartReducers from './cart.reducer/cart.reducers';
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducers,
 });
