7c2b97d4f00ffde1192c79d3b3e3bb6938c0257e
mail21 <ismailidn1@gmail/com>
2020-08-19 14:42:26 +0700

    adding react-persist

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,23 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user.reducer/user.reducer';
 import cartReducers from './cart.reducer/cart.reducers';
+import directoryReducer from './directory.reducer/directory.reducer';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// storage disini itu seperti localStorage tapi versi redux-persist
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['cart'],
+};
+//disini whitelist maksudnya jadi ya dipersist itu hanya state cart
+// state user tidak perlu karena sudah dipersist oleh firebase
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducers,
+  directory: directoryReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
