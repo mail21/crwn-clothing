edf42cde45445c03d8927ea525289f524db84d93
mail21 <ismailidn1@gmail.com>
2020-08-21 10:27:10 +0700

    Data normalisasi pada shop data, mengubah shop data dari array menajdi object

--- src/redux/shop.reducer/shop.selectors.js
+++ src/redux/shop.reducer/shop.selectors.js
@@ -1,22 +1,21 @@
 import { createSelector } from 'reselect';
 
-const COLLECTIONS_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const getCollections = createSelector([selectShop], (shop) => shop.collections);
+// const COLLECTIONS_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
 
-export const selectCollection = (urlParameter) => {
-  return createSelector([getCollections], (collections) =>
-    collections.find((collection) => collection.id === COLLECTIONS_ID_MAP[urlParameter])
-  );
-};
+// export const selectCollection = (urlParameter) => {
+//   return createSelector([getCollections], (collections) =>
+//     collections.find((collection) => collection.id === COLLECTIONS_ID_MAP[urlParameter])
+//   );
+// };
 
 /**
    kode selectCollection memang membingungkan jadi COLLECTIONS_ID_MAP[urlParameter]
@@ -29,3 +28,16 @@ export const selectCollection = (urlParameter) => {
    jadi nanti collection.id akan dibandingkan dengan angka stringnya
    lalu .find akan mengembalikan objek dari hasil perbandingannya
  */
+
+export const selectCollection = (urlParameter) => {
+  return createSelector([getCollections], (collections) => collections[urlParameter]);
+};
+
+/*
+  karena data dari shop_data sudah di normalisasi dan datanya sudah bukan array lagi 
+  melainkan objek jadinya kita hanya memanggil memberi keynya saja
+  urlParameter = mens atau womens atau sneakers dll
+  jadinya collections[sneakers] 
+  jadinya akan mereturn objek sneakers 
+  
+*/
